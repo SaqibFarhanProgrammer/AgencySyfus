bc03301223b7324a6cbe608ceeff0a773f2ea562
saqib <saqibkhatai@gmail.com>
2025-08-02 14:33:23 -0700

    add footer

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
       gsap.from(".word", {
         y: 80,
         opacity: 0,
-        duration: .5,
+        duration: 1,
         ease: "power3.out",
         stagger: 0.04,
         scrollTrigger: {
